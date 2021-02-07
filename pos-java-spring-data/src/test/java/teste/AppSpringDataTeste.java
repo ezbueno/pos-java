e091c885e3ea6b93649e6e726df91cd0be267193
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2021-02-07 00:51:46 -0300

    Criado a classe de para testar a conexão com o BD

---
+++ pos-java-spring-data/src/test/java/teste/AppSpringDataTeste.java
@@ -0,0 +1,16 @@
+package teste;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
+public class AppSpringDataTeste {
+	
+	@Test
+	public void inserir() {
+		System.out.println("Spring inicializado com sucesso!");
+	}
+}
