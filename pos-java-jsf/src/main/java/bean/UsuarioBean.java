e13dc15d96511797b04eda9c87033a0100bf1abc
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2021-02-16 22:33:34 -0300

    Finalizado a listagem de usuários

--- pos-java-jsf/src/main/java/bean/UsuarioBean.java
+++ pos-java-jsf/src/main/java/bean/UsuarioBean.java
@@ -1,5 +1,8 @@
 package bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 
@@ -12,6 +15,7 @@ public class UsuarioBean {
 
 	private Usuario usuario = new Usuario();
 	private DaoGenerico<Usuario> daoGenerico = new DaoGenerico<>();
+	private List<Usuario> lista = new ArrayList<>();
 
 	public Usuario getUsuario() {
 		return usuario;
@@ -21,4 +25,24 @@ public class UsuarioBean {
 		this.usuario = usuario;
 	}
 
+	public List<Usuario> getLista() {
+		lista = daoGenerico.listar(Usuario.class);
+		return lista;
+	}
+
+	public String salvar() {
+		daoGenerico.salvar(usuario);
+		return "";
+	}
+	
+	public String excluir() {
+		daoGenerico.deletar(usuario);
+		usuario = new Usuario();
+		return "";
+	}
+
+	public String novo() {
+		usuario = new Usuario();
+		return "";
+	}
 }
